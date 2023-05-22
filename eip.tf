acf618ec1513061bdedff40449bd89d7f95c29c5
mvg1k <116655825+mvg1k@users.noreply.github.com>
2023-05-22 22:51:40 +0300

    Delete eip.tf

--- eip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "static_ip" {
-  instance = aws_instance.grafana.id
-}
-
-# Вихідні дані Terraform для повернення значень
-output "instance_ip" {
-    value = aws_eip.static_ip.public_ip
-}
